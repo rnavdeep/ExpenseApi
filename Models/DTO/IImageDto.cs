a65bd594abb9b752521fc41f75526ca9912f5171
RANN1D1901 <navdeeprandhawa0001@gmail.com>
2024-09-29 17:22:05 +1000

    Removed Files Cleanup

--- Models/DTO/IImageDto.cs
+++ Models/DTO/IImageDto.cs
@@ -9,34 +9,6 @@ namespace Expense.API.Models.DTO
         public IFormFile File { get; set; }
         public string? FileDescription { get; set; }
     }
-    public class WalkImageDto : IDocDto
-    {
-        private IFormFile _file;
-        private string _fileName;
-        private string? _fileDescription;
-        private string _walkCode;
-        [Required]
-        public IFormFile File { get => _file; set => _file = value; }
-        [Required]
-        public string FileName { get => _fileName; set => _fileName = value; }
-        public string? FileDescription { get => _fileDescription; set => _fileDescription = value; }
-        [Required]
-        public string WalkCode { get => _walkCode; set => _walkCode = value; }
-    }
-    public class RegionImageDto : IDocDto
-    {
-        private IFormFile _file;
-        private string _fileName;
-        private string? _fileDescription;
-        private string _regionCode;
-        [Required]
-        public IFormFile File { get => _file; set => _file = value; }
-        [Required]
-        public string FileName { get => _fileName; set => _fileName = value; }
-        public string? FileDescription { get => _fileDescription; set => _fileDescription = value; }
-        [Required]
-        public string RegionCode { get => _regionCode; set => _regionCode = value; }
-    }
     public class DocumentDto: IDocDto
     {
         private IFormFile _file;
